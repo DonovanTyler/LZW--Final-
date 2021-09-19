8d0fa9e83f5dd8e9f88854480449924890e8d9cf
Tyler Donovan <89551524+DonovanTyler@users.noreply.github.com>
2021-09-19 11:16:21 -0700

    Fork

--- src/TesterLZW.java
+++ src/TesterLZW.java
@@ -6,16 +6,20 @@ import java.util.stream.Collectors;
 public class TesterLZW {
 	public static void main(String [] args) throws IOException 
 	{
+		long startTime = System.currentTimeMillis();
 		LZW john = new LZW ("smalltest.txt"); 
 		String compressed = john.compress(); 
 	
-		System.out.println(john.decompressFromByteFile("output.byte"));
+		//System.out.println(john.decompressFromByteFile("output.byte"));
 		
 		//Note: this throws an error because the decoded int string doesn't seem to be correct
 		List<Integer> sampleInput = Arrays.asList(97, 97, 98, 257, 259, 99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);;
 		//Proof that my decompression works:
 		System.out.println(john.decompressFromInput(sampleInput));
-		
+		long elapsedTime = System.currentTimeMillis() - startTime;
+		long elapsedSeconds = elapsedTime / 1000;
+		long secondsDisplay = elapsedSeconds % 60;
+		long elapsedMinutes = elapsedSeconds / 60;
 	   
 	}
 }
